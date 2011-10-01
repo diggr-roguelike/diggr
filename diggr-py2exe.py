ce213d16dfe3e614ebab931b6234fa12d8757989
tkatchev <tkatchev>
2011-10-01 09:41:13 +0400

    .

--- diggr-py2exe.py
+++ diggr-py2exe.py
@@ -7,7 +7,7 @@ setup(
   windows=[
         { "script": 'diggr.pyw', "icon_resources": [(1, "Rock-Collecting.ico")]},
         'diggr-replayer.pyw'],
-  data_files=[('.', ['terminal10x16_gs_ro.png', 'SDL.dll',
+  data_files=[('.', ['font.png', 'terminal10x16_gs_ro.png', 'SDL.dll',
                      'libtcod-gui-mingw.dll', 'libtcod-mingw.dll',
                      'GUIDE.txt'])],
   zipfile=None,
